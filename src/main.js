277fcbfb400f7d54d633c48801808cf969f21fd2
mariodai <daizhenghw@outlook.com>
2023-04-18 20:46:55 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import createStore from 'vuex'
 
 createApp(App).mount('#app')
+
+const initAxios = axios.create({
+    timeout:18000
+})
+initAxios.interceptors.request.use((config)=> {
+    return config
+})
+export default createStore({
+    state: {
+        num:111
+    }
+})
